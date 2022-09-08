cfd9ed28a7a6881679a94fcb108f5f73f67a4fbd
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-09-08 18:27:11 +0300

    Добавьте файлы проекта.

---
+++ Lesson4Framework/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4Framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            Console.Title = Properties.Settings.Default.ApplicationNameDebug;
+#else
+            Console.Title = Properties.Settings.Default.ApplicationName;
+#endif
+            Console.ReadKey();
+        }
+    }
+}
